0635e94b6ce61be116c1da28fbdac19639713f2d
Abhishek Kumar <kharwar.abhishek9@gmail.com>
2020-07-27 18:07:26 +0530

    - First commit (setup base project)

---
+++ HeadyTest/Product.swift
@@ -0,0 +1,13 @@
+//
+//  Product.swift
+//  HeadyTest
+//
+//  Created by Ashok Mahawar on 27/07/20.
+//  Copyright © 2020 Abhishek Kumar. All rights reserved.
+//
+
+import UIKit
+
+class Product: NSObject {
+
+}
